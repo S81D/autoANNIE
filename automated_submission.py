b7bddea5f09c6ae680503407f99f4479fe6c3e18
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-07-01 08:19:30 -0500

    Update automated_submission.py
    changes to account for overlap in the resubmission stage

--- automated_submission.py
+++ automated_submission.py
@@ -83,7 +83,7 @@ if missing == 'n':
         submit_jobs.run_container_job(run, name_TA, DLS, first, final)
 
         # We can then create the job_submit script that will send our job (with files) to the grid
-        submit_jobs.submit_grid_job(run, part_list[0][i], part_list[1][i], input_path, output_path, TA_tar_name, disk_space, raw_path, trig_path, beamfetcher_path)
+        submit_jobs.submit_grid_job(run, part_list[0][i], part_list[1][i], input_path, output_path, TA_tar_name, disk_space, raw_path, trig_path, beamfetcher_path, first, final)
 
         # Lastly, we can execute the job submission script and send the job to the grid
         os.system('sh submit_grid_job_' + run + '.sh')
@@ -125,81 +125,62 @@ if missing == 'y':
     print("Number of missing files: ", num_raw_files-num_processed_files)
     print("Missing processed files: ", missing_files)
 
+    print('\nResubmission step size: ' + str(step_size) + '\n')
 
-    # go through the missing processed files to group them if there are consecutive part files
-    def group_consecutive_elements(lst):
-        result = []
-        current_group = []
-        for i in range(len(lst)):
-            if i > 0 and lst[i] != lst[i - 1] + 1:
-                result.append(current_group)
-                current_group = []
-            current_group.append(lst[i])
-        if current_group:
-            result.append(current_group)
-        return result
+    first_part = min(missing_files)
+    last_part = max(missing_files)
 
-    missing_list = group_consecutive_elements(missing_files)
-    ml = 0
-    for i in range(len(missing_list)):
-        if len(missing_list[i]) > ml:
-            ml = len(missing_list[i])
-        
-    print('\nMaximum part bunch size = ' + str(ml))
-
-
-    auto_step_size = step_size        # pass different step size arg if running 'y' for missing
-
-    for l in range(len(missing_list)):
-
-        first_part = min(missing_list[l])
-        last_part = max(missing_list[l])
-
-        if len(missing_list[l]) >= auto_step_size:
-            step_size = auto_step_size
-        elif len(missing_list[l]) < auto_step_size:
-            step_size = len(missing_list[l])
-
-        part_list = [[], []]     # [0] = first,  [1] = final
-        for i in range(first_part, last_part + 1, step_size):
-                
-            if i != 0:
-                part_list[0].append(i-1)
-            else:
-                part_list[0].append(i)
-            if (i+step_size) > last_part:
-                if last_part < final_part:
-                    part_list[1].append(last_part+1)
-                else:
-                    part_list[1].append(final_part)
-            else:
-                part_list[1].append(i + step_size)
-
-
-        # calculate disk space requirements
-        import math
-        str(math.ceil(5 + .3*(step_size+2)))    # refined for beam runs based on job histories
-        #disk_space = str(10)     # typically fine as a default - good for Laser single part submissions
-
-        # Submit the entire batch through multiple jobs, based on the user input (above)
-
-        for i in range(len(part_list[0])):     # loop over number of jobs
+    part_list = [[], []]     # [0] = first,  [1] = final
+    for i in range(first_part, last_part + 1, step_size):
             
-            # create the run_container_job and grid_job scripts
-            os.system('rm grid_job_' + run + '.sh')
-            submit_jobs.grid_job(run, user, input_path, TA_tar_name, name_TA)
-            os.system('rm run_container_job_' + run + '.sh')
-            submit_jobs.run_container_job(run, name_TA, DLS)
+        if i != 0:
+            part_list[0].append(i-1)
+        else:
+            part_list[0].append(i)
+        if (i+step_size) > last_part:
+            if last_part < final_part:
+                part_list[1].append(last_part+1)
+            else:
+                part_list[1].append(final_part)
+        else:
+            part_list[1].append(i + step_size)
 
-            # We can then create the job_submit script that will send our job (with files) to the grid
-            os.system('rm submit_grid_job_' + run + '.sh')
-            submit_jobs.submit_grid_job(run, part_list[0][i], part_list[1][i], input_path, output_path, TA_tar_name, disk_space, raw_path, trig_path, beamfetcher_path)
 
-            # Lastly, we can execute the job submission script and send the job to the grid
-            os.system('sh submit_grid_job_' + run + '.sh')
-            print('\n# # # # # # # # # # # # # # # # # # # # #')
-            print('Run ' + run + ' p' + str(part_list[0][i]) + '-' + str(part_list[1][i]) + ' sent')
-            print('# # # # # # # # # # # # # # # # # # # # #\n')
+    # calculate disk space requirements
+    import math
+    str(math.ceil(5 + .3*(step_size+2)))    # refined for beam runs based on job histories
+    #disk_space = str(10)     # typically fine as a default - good for Laser single part submissions
+
+    # Submit the entire batch through multiple jobs, based on the user input (above)
+
+    first = False; final = False
+    n_jobs = len(part_list[0])
+
+    os.system('rm grid_job_' + run + '*.sh'); os.system('rm run_container_job_' + run + '*.sh'); os.system('rm submit_grid_job_' + run + '*.sh')
+    for i in range(len(part_list[0])):     # loop over number of jobs
+
+        if part_list[0][i] == 0:               # p0
+            first = True
+            sh_name = 'submit_grid_job_' + run + '_first.sh'
+        elif part_list[0][i+1] == final_part:  # final part of the run
+            final = True
+            sh_name = 'submit_grid_job_' + run + '_final.sh'
+        else:
+            sh_name = 'submit_grid_job_' + run + '.sh'
+            os.system('rm grid_job_' + run + '.sh'); os.system('rm run_container_job_' + run + '.sh'); os.system('rm submit_grid_job_' + run + '.sh')
+        
+        # create the run_container_job and grid_job scripts
+        submit_jobs.grid_job(run, user, input_path, TA_tar_name, name_TA, first, final)
+        submit_jobs.run_container_job(run, name_TA, DLS, first, final)
+
+        # We can then create the job_submit script that will send our job (with files) to the grid
+        submit_jobs.submit_grid_job(run, part_list[0][i], part_list[1][i], input_path, output_path, TA_tar_name, disk_space, raw_path, trig_path, beamfetcher_path, first, final)
+
+        # Lastly, we can execute the job submission script and send the job to the grid
+        os.system('sh submit_grid_job_' + run + '.sh')
+        print('\n# # # # # # # # # # # # # # # # # # # # #')
+        print('Run ' + run + ' p' + str(part_list[0][i]) + '-' + str(part_list[1][i]) + ' sent')
+        print('# # # # # # # # # # # # # # # # # # # # #\n')
 
 
 print('\nJobs successfully submitted!\n')
