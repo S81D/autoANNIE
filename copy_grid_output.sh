7bcaa089c3f25e03b2c11f659f9d562772df6bca
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-07-01 10:45:03 -0500

    Update copy_grid_output.sh
    Added function as to skip files that are already present in the transfer directory. Without this the copy hangs indefinitely (or at least for a very long time)

--- copy_grid_output.sh
+++ copy_grid_output.sh
@@ -5,33 +5,65 @@ data_path=$2
 output_path=$3
 lappd_EB_path=$4
 
+# Overwrite not enabled for ifdh cp - skip the file if it exists in /persistent
+file_exists() {
+    if [ -e "$1" ]; then
+        return 0  # File exists
+    else
+        return 1  # File does not exist
+    fi
+}
 
-# Copy processed files
-
+# create destination folders and make them editable/writable
 mkdir -p $data_path/R$run
 chmod 777 $data_path/R$run
+mkdir -p $lappd_EB_path/LAPPDTree/R$run
+chmod 777 $lappd_EB_path/LAPPDTree/R$run
+mkdir -p $lappd_EB_path/offsetFit/R$run
+chmod 777 $lappd_EB_path/offsetFit/R$run
 
-echo ""
-echo "Copying Processed Files..."
-echo ""
-
+# setup transfer
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setup
 setup ifdhc v2_5_4
 
-ifdh cp $output_path/$run/Processed* $data_path/R$run/
 
+# copy processed files
 
-# Copy LAPPD-related EB files (LAPPDTree + offsetFit result)
+echo ""
+echo "Copying ProcessedData Files..."
+echo ""
 
-mkdir -p $lappd_EB_path/LAPPDTree/R$run
-chmod 777 $lappd_EB_path/LAPPDTree/R$run
+# ProcessedData files
+for file in "$output_path/$run/Processed"*; do
+    filename=$(basename "$file")
+    if ! file_exists "$data_path/R$run/$filename"; then
+        ifdh cp "$file" "$data_path/R$run/"
+    else
+        echo "File $filename already exists in $data_path/R$run/. Skipping..."
+    fi
+done
 
-mkdir -p $lappd_EB_path/offsetFit/R$run
-chmod 777 $lappd_EB_path/offsetFit/R$run
+# LAPPD-related EB files (LAPPDTree + offsetFit result)
 
 echo ""
 echo "Copying LAPPD Files..."
 echo ""
 
-ifdh cp $output_path/$run/LAPPDTree* $lappd_EB_path/LAPPDTree/R$run/
-ifdh cp $output_path/$run/offsetFitResult* $lappd_EB_path/offsetFit/R$run/
+for file in "$output_path/$run/LAPPDTree"*; do
+    filename=$(basename "$file")
+    if ! file_exists "$lappd_EB_path/LAPPDTree/R$run/$filename"; then
+        ifdh cp "$file" "$lappd_EB_path/LAPPDTree/R$run/"
+    else
+        echo "File $filename already exists in $lappd_EB_path/LAPPDTree/R$run/. Skipping..."
+    fi
+done
+
+for file in "$output_path/$run/offsetFitResult"*; do
+    filename=$(basename "$file")
+    if ! file_exists "$lappd_EB_path/offsetFit/R$run/$filename"; then
+        ifdh cp "$file" "$lappd_EB_path/offsetFit/R$run/"
+    else
+        echo "File $filename already exists in $lappd_EB_path/offsetFit/R$run/. Skipping..."
+    fi
+done
+
