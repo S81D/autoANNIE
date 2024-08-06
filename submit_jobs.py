238762b64f76b31f6fc83c4f2b6802444f4bae3a
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-08-06 14:35:31 -0500

    Update submit_jobs.py

--- submit_jobs.py
+++ submit_jobs.py
@@ -542,7 +542,7 @@ def container_BC(name_TA, input_path):
     file.write('\n')
 
     file.write('FIRST_ARG=$1\n')
-    file.write('PART_NAME=$(echo "$FIRST_ARG" | grep -oE \'[0-9]+\'\n')
+    file.write('PART_NAME=$(echo "$FIRST_ARG" | grep -oE \'[0-9]+\')\n')
     file.write('PI=$2\n')
     file.write('PF=$3\n')
     file.write('\n')
