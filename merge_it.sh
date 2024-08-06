d2f054b5cd81282eca5af5a5304f12fa97e3fee6
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-08-06 10:57:00 -0500

    Update merge_it.sh
    reworked mergeBeamTrees to handle merging BeamCluster root files as well. This script will handle both cases without adding additional files

--- merge_it.sh
+++ merge_it.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 BIND=$1
-APP_PATH=$2
+PATH=$2
 RUN=$3
+WHICH=$4
+
+if [ "$WHICH" == "beamfetcher" ]; then
+    FILE_NAME="beamfetcher"
+    TREE_NAME="BeamTree"
+elif [ "$WHICH" == "BC" ]; then
+    FILE_NAME="BeamCluster/BeamCluster"
+    TREE_NAME="Event"
+fi
 
 singularity shell ${BIND} /cvmfs/singularity.opensciencegrid.org/anniesoft/toolanalysis\:latest/ << EOF
 
-    root -l -q 'mergeBeamTrees.C("$APP_PATH", $RUN)'
+    root -l -q 'mergeBeamTrees.C("$PATH", $RUN, "$FILE_NAME", "$TREE_NAME")'
 
     .q
 
