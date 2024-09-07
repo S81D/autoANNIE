e156bfbccc6dfa438c7c3645b3f4a33fdf77775f
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-09-07 13:50:54 -0500

    Update master_script.py

--- master_script.py
+++ master_script.py
@@ -422,9 +422,9 @@ if which_mode == '2':        # BeamCluster
                     
                     # Filtered files (LAPPD + MRD)
                     print('\nTransferring Filtered datasets (MRD + LAPPD)...\n')
-                    os.system('sh BeamCluster/BC_copy.sh ' + runs_to_run[i] + ' ' + beamcluster_path + ' ' + scratch_path + ' ' + 'LAPPD' + ' ' + lappd_BC_path + ' ' + output_path + ' ' + lappd_filter_path + ' ' + mrd_filter_path)
+                    os.system('sh BeamCluster/BC_copy.sh ' + runs_to_run[i] + ' ' + beamcluster_path + ' ' + scratch_path + ' ' + 'LAPPD_filter' + ' ' + lappd_BC_path + ' ' + output_path + ' ' + lappd_filter_path + ' ' + mrd_filter_path)
                     time.sleep(1)
-                    os.system('sh BeamCluster/BC_copy.sh ' + runs_to_run[i] + ' ' + beamcluster_path + ' ' + scratch_path + ' ' + 'LAPPD' + ' ' + lappd_BC_path + ' ' + output_path + ' ' + lappd_filter_path + ' ' + mrd_filter_path)
+                    os.system('sh BeamCluster/BC_copy.sh ' + runs_to_run[i] + ' ' + beamcluster_path + ' ' + scratch_path + ' ' + 'MRD_filter' + ' ' + lappd_BC_path + ' ' + output_path + ' ' + lappd_filter_path + ' ' + mrd_filter_path)
                     time.sleep(1)
                     
                 else:
