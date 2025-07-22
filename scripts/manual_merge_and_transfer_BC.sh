7bb6a69cba8c9b26817f93223dd2a863f37ac3b0
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-07-22 09:10:30 -0500

    Update manual_merge_and_transfer_BC.sh

--- scripts/manual_merge_and_transfer_BC.sh
+++ scripts/manual_merge_and_transfer_BC.sh
@@ -3,13 +3,18 @@
 # if some pesky BeamCluster jobs are holding and you want to initiate the merge + transfer, run this script!
 # Usage: sh manual_merge_and_transfer_BC.sh <run>
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: sh manual_merge_and_transfer_BC.sh <run>"
+    exit 1
+fi
+
 RUN=$1
 
 # edit accordingly
 
 USER=<USER>
 
-SINGULARITY_COMMAND="-B/pnfs:/pnfs,/exp/annie/app/users/${RUN}/temp_directory:/tmp,/exp/annie/data:/exp/annie/data,/exp/annie/app:/exp/annie/app"
+SINGULARITY_COMMAND="-B/pnfs:/pnfs,/exp/annie/app/users/${USER}/temp_directory:/tmp,/exp/annie/data:/exp/annie/data,/exp/annie/app:/exp/annie/app"
 BC_PATH=/pnfs/annie/persistent/processed/processingData_EBV2/BeamClusterTrees/
 SCRATCH_PATH=/pnfs/annie/scratch/users/${USER}/autoANNIE/
 LAPPD_BC_PATH=/pnfs/annie/persistent/processed/processingData_EBV2/BeamClusterTrees/LAPPDBeamClusterTrees/
@@ -21,12 +26,12 @@ MRD_FILTER_PATH=/pnfs/annie/persistent/processed/processingData_EBV2/processed_E
 # - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # first merge the BC files
 
-# BC
-sh ../lib/merge_it.sh "$SINGULARITY_COMMAND" "$BC_SCRATCH_OUTPUT_PATH" "$RUN" BC
+# BC                    --> singularity arg is passed differently to preserve the string structure in quotes for proper bind-mounting when we enter the container
+sh lib/merge_it.sh ${SINGULARITY_COMMAND} "$BC_SCRATCH_OUTPUT_PATH" "$RUN" BC
 sleep 1
 
 # LAPPD
-sh ../lib/merge_it.sh "$SINGULARITY_COMMAND" "$BC_SCRATCH_OUTPUT_PATH" "$RUN" LAPPD
+sh lib/merge_it.sh ${SINGULARITY_COMMAND} "$BC_SCRATCH_OUTPUT_PATH" "$RUN" LAPPD
 sleep 1
 
 
@@ -34,11 +39,11 @@ sleep 1
 # copy merged and filtered files (../BeamCluster/BC_copy.sh) path assumes you're working out of the scripts/ subfolder
 
 # BC
-sh ../BeamCluster/BC_copy.sh ${RUN} ${BC_PATH} ${SCRATCH_PATH} BC ${LAPPD_BC_PATH} ${BC_SCRATCH_OUTPUT_PATH} ${LAPPD_FILTER_PATH} ${MRD_FILTER_PATH}
+sh BeamCluster/BC_copy.sh ${RUN} ${BC_PATH} ${SCRATCH_PATH} BC ${LAPPD_BC_PATH} ${BC_SCRATCH_OUTPUT_PATH} ${LAPPD_FILTER_PATH} ${MRD_FILTER_PATH}
 sleep 1
 
 # LAPPD BC
-sh ../BeamCluster/BC_copy.sh ${RUN} ${BC_PATH} ${SCRATCH_PATH} LAPPD ${LAPPD_BC_PATH} ${BC_SCRATCH_OUTPUT_PATH} ${LAPPD_FILTER_PATH} ${MRD_FILTER_PATH}
+sh BeamCluster/BC_copy.sh ${RUN} ${BC_PATH} ${SCRATCH_PATH} LAPPD ${LAPPD_BC_PATH} ${BC_SCRATCH_OUTPUT_PATH} ${LAPPD_FILTER_PATH} ${MRD_FILTER_PATH}
 sleep 1
 
 # LAPPD_filter
